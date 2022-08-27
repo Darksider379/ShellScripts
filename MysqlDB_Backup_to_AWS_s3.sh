ac3697c04af7558622a56c2266cf7fe8288e694d
Chandra Mukherjee <89653407+Darksider379@users.noreply.github.com>
2022-08-27 22:28:26 +0530

    Update MysqlDB_Backup_to_AWS_s3.sh

--- MysqlDB_Backup_to_AWS_s3.sh
+++ MysqlDB_Backup_to_AWS_s3.sh
@@ -4,15 +4,14 @@ DB_BACKUP="$DATE"_MYSQL_backup.sql
 
 export AWS_ACCESS_KEY_ID=AKIAZOEO5UZMMAKG672Z
 
-USERNAME=$1
-DB_PASSWORD=$2
-DB_HOST=$3
-DB_NAME=$4
-AWS_SECRET=$5
-BUCKET_NAME=$6
+DB_PASSWORD=$1
+DB_HOST=$2
+DB_NAME=$3
+AWS_SECRET=$4
+BUCKET_NAME=$5
 echo "Starting of script.."
 echo "Proceeding with mysql db backup.."
-mysqldump -u $USERNAME -p$DB_PASSWORD -h $DB_HOST $DB_NAME > /tmp/$DB_BACKUP && \
+mysqldump -u root $USERNAME -p$DB_PASSWORD -h $DB_HOST $DB_NAME > /tmp/$DB_BACKUP && \
 echo "Backup taken, proceeding with uploading to AWS S3 bucket.." && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
 aws s3 cp /tmp/$DB_BACKUP s3://$BUCKET_NAME/$DB_BACKUP
